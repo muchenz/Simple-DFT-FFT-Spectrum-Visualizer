947e017c1e6ffaa38de3472b8b090c2eb81c070d
p c <muchenz@poczta.onet.pl>
2024-02-11 22:48:48 +0100

    Add hook for better dpi handling

--- Wpf FFT/View/MainWindow.xaml.cs
+++ Wpf FFT/View/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -31,7 +32,34 @@ namespace Wpf_FFT.View
         {
             InitializeComponent();
             DataContext = mainWindowVM;
-            
+        }
+
+
+        //hook for scaling factor 
+
+        [DllImport("gdi32.dll")]
+        static extern int GetDeviceCaps(IntPtr hdc, int nIndex);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetDC(IntPtr hWnd);
+
+        const int LOGPIXELSX = 88;
+        const int LOGPIXELSY = 90;
+        protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
+        {
+            IntPtr hdc = GetDC(IntPtr.Zero);
+
+            var xDPI = GetDeviceCaps(hdc, LOGPIXELSX);
+
+            //var yDPI = GetDeviceCaps(hdc, LOGPIXELSY);
+
+
+            var windowsScalingFactor = xDPI / 96.0;
+
+            var fixingFactor = 1.01 + ((windowsScalingFactor-1) * 0.78);
+
+            ColumnMainWindow1.Width = new GridLength(sizeInfo.NewSize.Width * fixingFactor - ColumnMainWindow0.Width.Value);
+
         }
     }
 }
